85d3b97bd90f02374213afc16f2020638e57fe42
himanshu888ux <142731772+himanshu888ux@users.noreply.github.com>
2024-11-10 09:59:23 +0530

    Created using Colab

---
+++ plant_disease_detection.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPdem8TD7T/wlu1TFZ/opMh",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/himanshu888ux/first/blob/main/plant_disease_detection.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "FTYyJUIdU72S"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
